c3cbc6934ede8b5788fe6d708472ec78194e5a44
yeshufeng <yeshufeng@zbj.com>
2018-03-14 22:13:50 +0800

    增加zookeeper实现分布式锁的代码

--- src/main/java/com/zk/lock/DistributedLock.java
+++ src/main/java/com/zk/lock/DistributedLock.java
@@ -1,13 +1,9 @@
 package com.zk.lock;
 
-import com.zk.exception.LockException;
-import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
@@ -20,79 +16,111 @@ import org.apache.zookeeper.Watcher.Event.KeeperState;
  * @title
  * @date 2018/2/28
  */
-public class DistributedLock implements Lock,Watcher {
+public class DistributedLock{
+
+    // 超时时间
+    private static final int SESSION_TIMEOUT = 2000;
+    // zookeeper server列表
+    private String hosts = "127.0.0.1:2181";
+    private String groupNode = "locks";
+    private String subNode = "item";
 
     private ZooKeeper zk;
+    // 当前client创建的子节点
+    private String thisPath;
+    // 当前client等待的子节点
+    private String waitPath;
 
-    private String root = "/locks";//root目录
+    private CountDownLatch latch = new CountDownLatch(1);
 
-    private String lockName;//竞争资源的标志
+    /**
+     * 建立链接
+     */
+    public void connectZookeeper() throws Exception {
+        zk = new ZooKeeper(hosts, SESSION_TIMEOUT, new Watcher() {
+            public void process(WatchedEvent event) {
+                try {
+                    // 连接建立时, 打开latch, 唤醒wait在该latch上的线程
+                    if (event.getState() == KeeperState.SyncConnected) {
+                        latch.countDown();
+                    }
 
-    private String waitNode;//等待前一个锁
-
-    private String myZnode;//当前锁
-
-    private CountDownLatch latch;//计数器
-    private CountDownLatch connectedSignal=new CountDownLatch(1);
-    private int sessionTimeout = 30000;
-
-
-    public DistributedLock(String config, String lockName){
-        this.lockName = lockName;
-        // 创建一个与服务器的连接
-        try {
-            zk = new ZooKeeper(config, sessionTimeout, this);
-            connectedSignal.await();
-            Stat stat = zk.exists(root, false);//此处不执行 Watcher
-            if(stat == null){
-                // 创建根节点
-                zk.create(root, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                    // 发生了waitPath的删除事件
+                    if (event.getType() == Event.EventType.NodeDeleted && event.getPath().equals(waitPath)) {
+                        doSomething();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            throw new LockException(e);
-        } catch (KeeperException e) {
-            throw new LockException(e);
-        } catch (InterruptedException e) {
-            throw new LockException(e);
+        });
+
+        // 等待连接建立
+        latch.await();
+
+        // 创建子节点
+        thisPath = zk.create("/" + groupNode + "/" + subNode, null, ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        Thread.sleep(10);
+
+        //没有必要监听"/locks"的子节点的变化情况，防止惊群事件发生
+        List<String> childrenNodes = zk.getChildren("/" + groupNode, false);
+
+        if (childrenNodes.size() == 1) {
+            doSomething();
+        } else {
+            String thisNode = thisPath.substring(("/" + groupNode + "/").length());
+            // 排序
+            Collections.sort(childrenNodes);
+            int index = childrenNodes.indexOf(thisNode);
+            if (index == -1) {
+            } else if (index == 0) {
+                doSomething();
+            } else {
+                // 获得排名比thisPath前1位的节点
+                this.waitPath = "/" + groupNode + "/" + childrenNodes.get(index - 1);
+
+                // 在waitPath上注册监听器
+                zk.getData(waitPath, true, new Stat());
+            }
         }
     }
 
+    /**
+     * 共享资源的访问逻辑写在这个方法中
+     */
+    private void doSomething() throws Exception {
+        try {
+            System.out.println("gain lock: " + thisPath);
+            Thread.sleep(2000);//模拟操作
+        } finally {
+            System.out.println("finished: " + thisPath);
 
-    public void lock() {
-
-    }
-
-    public void lockInterruptibly() throws InterruptedException {
-
-    }
-
-    public boolean tryLock() {
-        return false;
-    }
-
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
-
-    public void unlock() {
-
-    }
-
-    public Condition newCondition() {
-        return null;
-    }
-
-    public void process(WatchedEvent watchedEvent) {
-        //建立连接用
-        if(watchedEvent.getState()== KeeperState.SyncConnected){
-            connectedSignal.countDown();
-            return;
-        }
-        //其他线程放弃锁的标志
-        if(this.latch != null) {
-            this.latch.countDown();
+            // 将thisPath删除, 监听thisPath的client将获得通知
+            zk.delete(this.thisPath, -1);
         }
     }
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            new Thread() {
+                public void run() {
+                    try {
+                        DistributedLock dl = new DistributedLock();
+                        dl.connectZookeeper();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+
+        Thread.sleep(Long.MAX_VALUE);
+
+    }
+
+
 
 }
 
